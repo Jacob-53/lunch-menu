b545a6c52bf49af0e4be95ec4dba7770c7e19889
jacob-53 <jacob8753@gmail.com>
2025-02-04 12:21:10 +0900

    lunch menu with melt

--- App.py
+++ App.py
@@ -1,10 +1,26 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 st.write("""
 # It's my first app
-Hello *Seoul*
+## 밥이 좋아  **Let's grab a bite and then finish this up**
 
-![img](https://149990825.v2.pressablecdn.com/wp-content/uploads/2023/09/Seoul1.jpg)
 """)
+
+df = pd.read_csv('note/menu.csv')
+
+start_idx = df.columns.get_loc('2025-01-07')
+mdf = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2], 
+                     var_name='dt', value_name='menu')
+
+sdf=mdf.replace(["-","x","<결석>"], pd.NA)
+adf=sdf.dropna()
+gdf=adf.groupby('ename')['menu'].count().reset_index()
+
+fig, ax = plt.subplots()
+gdf.plot(x='ename',y='menu',kind='bar',ax=ax)
+st.pyplot(fig)
 
 
