f8338831b081e70169d0ee66e1d3a3b04713d025
jacob-53 <jacob8753@gmail.com>
2025-02-04 15:33:03 +0900

    modify streamlit

--- App.py
+++ App.py
@@ -3,11 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-st.write("""
-# It's my first app
-## 밥이 좋아  **Let's grab a bite and then finish this up**
+st.title("점심 뭐 먹었나요?")
 
-""")
+st.subheader("입력")
+menu_name= st.text_input("메뉴 이름", placeholder="예: 참치김밥")
+member_name = st.text_input("먹은 사람", value="jacob")
+dt = st.date_input("먹은날짜")
+
+st.write("""# **Let's grab a bite and then finish this up** """)
 
 df = pd.read_csv('note/menu.csv')
 
@@ -18,6 +21,8 @@ mdf = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2],
 sdf=mdf.replace(["-","x","<결석>"], pd.NA)
 adf=sdf.dropna()
 gdf=adf.groupby('ename')['menu'].count().reset_index()
+
+gdf
 
 fig, ax = plt.subplots()
 gdf.plot(x='ename',y='menu',kind='bar',ax=ax)
