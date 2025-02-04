11e2c09c249d24f004d6037d46a422f6d6f4e22b
jacob-53 <jacob8753@gmail.com>
2025-02-04 18:16:27 +0900

    bulk insert

--- App.py
+++ App.py
@@ -44,6 +44,18 @@ if isPress:
     else:
         st.warning(f"모든 값을 입력하세요")
 
+st.subheader("Result check")
+query = "select menu_name as menu,member_name as ename,dt from lunch_menu order by dt desc"
+
+conn = get_connection()
+cursor = conn.cursor()
+cursor.execute(query)
+rows = cursor.fetchall()
+cursor.close()
+
+
+selected_df = pd.DataFrame(rows,columns=['menu','ename','dt'])
+selected_df
 
 df = pd.read_csv('note/menu.csv')
 
@@ -53,12 +65,35 @@ mdf = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2],
 
 sdf=mdf.replace(["-","x","<결석>"], pd.NA)
 adf=sdf.dropna()
-gdf=adf.groupby('ename')['menu'].count().reset_index()
+#gdf=adf.groupby('ename')['menu'].count().reset_index()
 
+gdf=selected_df.groupby('ename')['menu'].count().reset_index()
 gdf
 
 fig, ax = plt.subplots()
 gdf.plot(x='ename',y='menu',kind='bar',ax=ax)
 st.pyplot(fig)
 
+st.subheader("벌크인서트")
+isPress = st.button("한방에 인서트")
 
+if isPress:
+    conn = get_connection()
+    cursor = conn.cursor()
+    df = pd.read_csv('note/menu.csv')
+    start_idx = df.columns.get_loc('2025-01-07')
+    mdf = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2], var_name='dt', value_name='menu')
+
+    sdf=mdf.replace(["-","x","<결석>"], pd.NA)
+    adf=sdf.dropna()
+    blm = []
+    for i in adf.index: 
+        ename = adf.loc[i, "ename"]
+        dt = adf.loc[i, "dt"]
+        value = adf.loc[i, "menu"]
+        blm.append((value,ename,dt))
+    
+    cursor.executemany("INSERT INTO lunch_menu (menu_name,member_name,dt) VALUES (%s,%s,%s)",blm)
+    conn.commit()
+    cursor.close()
+    st.success(f"벌크 인서트 완료")
