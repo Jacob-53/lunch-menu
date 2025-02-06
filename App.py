dc84c8f16a8f9506aa99f2f77039543dc442770c
jacob-53 <jacob8753@gmail.com>
2025-02-06 22:41:18 +0900

    search agents

--- App.py
+++ App.py
@@ -63,8 +63,8 @@ if isPress:
             st.error(f"입력 실패")
     else:
         st.warning(f"모든 값을 입력하세요")
-cdt = st.date_input("언제?")
-chekPress = st.button("입력 안 한 사람 누구냐?")
+cdt = st.date_input("조회 할 날짜")
+chekPress = st.button("입력 안 한 요원 누구냐?")
 queryy = """
 select
     m.name,
@@ -73,7 +73,7 @@ from
 	member m
 left join lunch_menu l
 on 
-	l.member_id = m.id and l.dt = '{cdt}'
+	l.member_id = m.id and l.dt = %s
 group by
 	m.id, m.name
 having 
@@ -87,10 +87,17 @@ if chekPress:
         cursor = conn.cursor()
         cursor.execute(queryy,(cdt,))
         rowss = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        st.success(f"조회 성공")
-        st.write(rowss)  
+        fdf=pd.DataFrame(rowss,columns=['name','count'])
+        sunsinagents=fdf['name'].tolist()
+        if len(sunsinagents) >=1:
+            #st.text(",  ".join(sunsinagents))
+            cursor.close()
+            conn.close()
+            st.success(",  ".join(sunsinagents))
+        else:
+            cursor.close()
+            conn.close()
+            st.warning("모든 요원 입력 완료!")
     except Exception as e:
         st.warning(f"조회 중 오류가 발생했습니다")
         print(f"Exception: {e}")
@@ -153,20 +160,35 @@ if onePress:
         blm = []
         
         members = {"SEO": 5, "TOM": 1, "cho": 2, "hyun": 3, "nuni": 10, "JERRY": 4, "jacob": 7, "jiwon": 6, "lucas": 9, "heejin": 8}
+        success_cnt = 0
+        fail_cnt = 0
+
         for i in adf.index: 
             ename = adf.loc[i, "ename"]
             dt = adf.loc[i, "dt"]
             value = adf.loc[i, "menu"]
             blm.append((value,members[ename],dt))
     
-        cursor.executemany("INSERT INTO lunch_menu (menu_name,member_id,dt) VALUES (%s,%s,%s)",blm)
-        conn.commit()
-        cursor.close()
-        conn.close()
-        st.success("벌크 인서트 완료")
+        cursor.executemany("INSERT INTO lunch_menu (menu_name,member_id,dt) VALUES (%s,%s,%s) ON CONFLICT (member_id,dt)  DO NOTHING",blm)
+        
+        success_cnt = cursor.rowcount
+        fail_cnt = len(blm) - success_cnt
+        
+        if success_cnt == len(blm):
+            conn.commit()
+            cursor.close()
+            conn.close()
+            st.success(f"벌크 인서트 완료 총 {success_cnt} 건")
+        
+        else:
+            st.warning(f"총 {len(blm)} 건 중 {fail_cnt} 건 실패")       
+            conn.commit()
+            cursor.close()
+            conn.close()
+    
     except Exception as e: 
         conn.rollback()
         cursor.close()
         conn.close()
-        st.error("데이터가 중복되어 실행을 취소합니다")
+        st.error(f"오류 발생 ; {e} ")
         print(f"Exception: {e}")
