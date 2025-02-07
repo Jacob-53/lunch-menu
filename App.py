4b3450a42547684808618fc7db742e91c6fc5987
jacob-53 <jacob8753@gmail.com>
2025-02-07 18:17:35 +0900

    modify app.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ from lunch_menu.db import get_connection, insert_menu, select_table, menu_plot
 st.set_page_config(page_title="점심 Check!! 뭐 먹었나요?", page_icon="🍱")
 
 st.title("점심 뭐 먹었나요?")
-st.markdown("# Let's grab a bite and then finish this up")
-imageurl= https://patchesofpride.wordpress.com/wp-content/uploads/2016/04/homerdoughnuts.png
-st.image(imageurl, use_column_width=True)
+st.markdown("# Let's grab a bite and then finish this up") 
+imageurl = "https://patchesofpride.wordpress.com/wp-content/uploads/2016/04/homerdoughnuts.png"
+st.image(imageurl) 
 st.sidebar.header("점심 뭐 먹었나요?")
